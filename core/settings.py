a70553a49f633ea9dbf7e04af4c60f703ea0fda1
Emir Amangulov <amangulov03@gmail.com>
2025-10-22 23:12:39 +0600

    -

--- core/settings.py
+++ core/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-qt&etwoc&7gn!vhf2p85d=*2ts71!0*r24am-6)26fk#1*f#ex'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
